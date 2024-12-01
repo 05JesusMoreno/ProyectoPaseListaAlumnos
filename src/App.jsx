6eafea779a3f904de43c30c98143743532244564
Julio <julio.ce01@hotmail.com>
2024-12-01 16:23:20 -0600

    cambios 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Inicio from "./views/inicio";
 
@@ -15,21 +14,6 @@ function App() {
           <Route path="/" element={<Inicio />} />
         </Routes>
     </BrowserRouter>
-=======
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div className="text-blue-400 text-3xl font-semibold text-center">
-        hola mundo
-      </div>
-    </>
->>>>>>> ab0ad0f29b655945d5703bc4b6470cc32b6c6136
   );
 }
 
